b2d30f4e2e226ee828990663c513df26cea8eb4b
Audrea Cook <aud@codewitch.dev>
2021-06-09 09:25:36 -0600

    Add Header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Calculator from "./components/Calculator";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Calculator />
       <Footer />
     </div>
