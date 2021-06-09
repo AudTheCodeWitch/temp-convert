b671b1ddb4d14a6bef0ac23ac9a04d8ad3a807c3
Audrea Cook <aud@codewitch.dev>
2021-06-09 15:07:41 -0600

    Add color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poiret': ['Poiret One', 'cursive']
+      },
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+        accent: 'var(--color-bg-accent)'
+      },
+      textColor: {
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+        accent: 'var(--color-text-accent)'
+      },
+      borderColor: {
+        primary: 'var(--color-border-primary)'
+      }
+    },
   },
   variants: {
     extend: {},
