afb04820a889e97cfc69bf2ad5273220b79125d8
Audrea Cook <aud@codewitch.dev>
2021-06-09 15:06:58 -0600

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Toggle from "./Toggle";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Temp Convert</h1>
+    <header className={'w-4/5 mx-auto my-4 text-primary flex flex-row'}>
+      <h1 className={'text-3xl font-poiret flex-grow self-center'}>Temp Convert</h1>
+      <Toggle />
     </header>
   );
 };
