9876037e9270cd8ae0a10d7af4511fdae7f94a33
Audrea Cook <aud@codewitch.dev>
2021-06-09 15:02:11 -0600

    Add degree images

--- src/components/TempInput.js
+++ src/components/TempInput.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
+import Degree_F from '../assets/degree_f.svg';
+import Degree_C from '../assets/degree_c.svg'
 
 const scaleNames = {
   c: 'Celsius',
   f: 'Fahrenheit'
+}
+
+const images = {
+  c: Degree_C,
+  f: Degree_F
 }
 
 class TempInput extends Component {
